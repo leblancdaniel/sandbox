6cf0c02c9133a3134a9b373e2108913ebda42c6c
leblancdaniel <daniel.leblanc@augustusai.com>
2020-04-17 09:55:18 -0400

    numerical feature engineering

--- tutorials/ML/feature_engineering.py
+++ tutorials/ML/feature_engineering.py
@@ -1,14 +1,25 @@
-import os
 import pandas as pd
 from sklearn import preprocessing, metrics
 import lightgbm as lgb
 import category_encoders as ce
+import itertools
 
 # Load data
 script_dir = os.path.dirname(__file__)
 rel_path = "train_sample.csv"
 filepath = os.path.join(script_dir, rel_path)
 click_data = pd.read_csv(filepath, parse_dates=['click_time'])
+click_times = click_data['click_time']
+clicks = click_data.assign(day=click_times.dt.day.astype('uint8'),
+                           hour=click_times.dt.hour.astype('uint8'),
+                           minute=click_times.dt.minute.astype('uint8'),
+                           second=click_times.dt.second.astype('uint8'))
+
+# Label encoding for categorical features
+cat_features = ['ip', 'app', 'device', 'os', 'channel']
+for feature in cat_features:
+    label_encoder = preprocessing.LabelEncoder()
+    clicks[feature] = label_encoder.fit_transform(clicks[feature])
 
 # Dataset split helper function
 def get_data_splits(dataframe, valid_fraction=0.1):
@@ -16,18 +27,11 @@ def get_data_splits(dataframe, valid_fraction=0.1):
         the column 'click_time'. Set the size of the validation and test sets with
         the valid_fraction keyword argument.
     """
-    # Parse date feature
-    clicks = dataframe.copy()
-    clicks['day'] = clicks['click_time'].dt.day.astype('uint8')
-    clicks['hour'] = clicks['click_time'].dt.hour.astype('uint8')
-    clicks['minute'] = clicks['click_time'].dt.minute.astype('uint8')
-    clicks['second'] = clicks['click_time'].dt.second.astype('uint8')
-    clicks = clicks.sort_values('click_time')
+    dataframe = dataframe.sort_values('click_time')
     valid_rows = int(len(dataframe) * valid_fraction)
-    train = clicks[:-valid_rows * 2]
-    # valid size == test size, last two sections of the data
-    valid = clicks[-valid_rows * 2:-valid_rows]
-    test = clicks[-valid_rows:]
+    train = dataframe[:-valid_rows * 2]
+    valid = dataframe[-valid_rows * 2:-valid_rows]
+    test = dataframe[-valid_rows:]
     
     return train, valid, test
 
@@ -58,13 +62,13 @@ def train_model(train, valid, test=None, feature_cols=None):
         return bst, valid_score
 
 print("Baseline model")
-train, valid, test = get_data_splits(click_data)
+train, valid, test = get_data_splits(clicks)
 _ = train_model(train, valid)
-
+"""
 print("CountEncoder model")
 # Create new columns in clicks using encoder
 cat_features = ['ip', 'app', 'device', 'os', 'channel']
-train, valid, test = get_data_splits(click_data)
+train, valid, test = get_data_splits(clicks)
 # create and train CountEncoder on categorcal columns
 count_enc = ce.CountEncoder(cols=cat_features)
 count_enc.fit(train[cat_features])
@@ -77,7 +81,7 @@ _ = train_model(train_encoded, valid_encoded)
 print("TargetEncoder model")
 # Remove 'ip' column to improve TargetEncoder and CatBoostEncoder results
 cat_features = ['app', 'device', 'os', 'channel']
-train, valid, test = get_data_splits(click_data)
+train, valid, test = get_data_splits(clicks)
 # Create the target encoder. You can find this easily by using tab completion.
 target_enc = ce.TargetEncoder(cols=cat_features)
 target_enc.fit(train[cat_features], train['is_attributed'])
@@ -89,7 +93,7 @@ _ = train_model(train_encoded, valid_encoded)
 
 print("CatBoostEncoder model")
 # Do the same for CatBoostEncoder
-train, valid, test = get_data_splits(click_data)
+train, valid, test = get_data_splits(clicks)
 # Create the CatBoost encoder
 cb_enc = ce.CatBoostEncoder(cols=cat_features)
 # Learn encoding from the training set
@@ -99,3 +103,39 @@ train_encoded = train.join(cb_enc.transform(train[cat_features]).add_suffix('_cb
 valid_encoded = valid.join(cb_enc.transform(valid[cat_features]).add_suffix('_cb'))
 # Train model on CatBoost encoded datasets
 _ = train_model(train_encoded, valid_encoded)
+"""
+# Feature generation based on interactions
+cat_features = ['ip', 'app', 'device', 'os', 'channel']
+interactions = pd.DataFrame(index=clicks.index)
+# Iterate through each pair of features, combine them into interaction features
+for c1, c2 in itertools.combinations(cat_features, 2):
+    new_col_name = '_'.join([c1, c2])
+    new_values = clicks[c1].map(str) + "_" + clicks[c2].map(str)
+    encoder = preprocessing.LabelEncoder()
+    interactions[new_col_name] = encoder.fit_transform(new_values)
+# combine interaction features with clicks dataset
+clicks = clicks.join(interactions)
+
+# Generate numerical features based on rolling window
+# Number of events in the past X hours
+def count_past_events(series, window='6H'):
+    """ Returns a series that counts the number of events in the past 6 hours """
+    series = pd.Series(series.index, index=series)
+    past_events = series.rolling(window).count() - 1
+    return past_events
+clicks['ip_past_6H_counts'] = count_past_events(clicks['ip'], '6H')
+# Time since last event
+def time_diff(series):
+    """ Returns a series with the time since the last timestamp in seconds """
+    return series.diff().dt.total_seconds()
+timedeltas = clicks.groupby('ip')['click_time'].transform(time_diff)
+clicks['past_events_6H'] = timedeltas
+# Number of previous app downloads
+def previous_attributions(series):
+    """ Returns a series with the rolling sum of target series since current row """
+    sums = series.expanding(min_periods=2).sum() - series
+    return sums
+clicks['ip_past_6hr_counts'] = previous_attributions(clicks['is_attributed'])
+# split and train model on new features: interactions, past counts, time since last, rolling sum
+train, valid, test = get_data_splits(clicks.join(timedeltas))
+_ = train_model(train, valid, test)
